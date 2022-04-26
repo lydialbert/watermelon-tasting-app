ce42a093f16aaadf6b04dd53dbe6595ca4c3bdcb
Lydia Albert <lydiaalbert@icloud.com>
2022-04-26 12:48:14 -0700

    Initialized Watermelon Tasting App.

---
+++ model.py
@@ -0,0 +1,25 @@
+# Database for Watermelon Tasting 
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.model):
+    """A User and their Watermelon Reservations."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Coulmn(db.String, nullable=False, unique=True)
+    fname = db.Column(db.String, nullable=False)
+    lname = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"<User's First Name={self.fname}, User's Username={self.user_name}>"
+
+
+if __name__ == "__main__":
+    from server import app
+    
+    connect_to_db(app)
+    db.create_all()
---
+++ to-do.sh
@@ -0,0 +1,21 @@
+To-Do
+
+Login (by username only)
+Search Form (pick a date {optional time range}) --> Avaiable reservations 
+    Reservation details:
+        start and end on half hour or hour 
+        Are 30 mins
+        Only 1 per month!
+        Only book 1 reservation at a time, add feature of error message if trying to book two at one time
+    No reservations --> flash a message saying that
+Click on appointment to book an appointment (using javascript for an event listener)
+Page that shows all reservations (like my bucketlists page)
+
+
+
+Create a database, called users reservations
+Four Pages:
+    Login screen
+    Page to search for appointments
+    Results page to view the results (then click to book an appointment)
+    Page to view all schedule appointments (optional - edit/cancel appiontment feature)
